217aff4f374beac0c323bbed52ea032d1e43877b
Reyna <canche26rey@gmail.com>
2024-01-10 12:20:29 -0600

    restaurar la estructura

--- PageWebProfGloria/src/Componentes/Acerca.jsx
+++ PageWebProfGloria/src/Componentes/Acerca.jsx
@@ -13,7 +13,7 @@ const Acerca = () => {
       Multiple lines of text that form the lede, informing new readers quickly
       and efficiently about what’s most interesting in this post’s contents.
     </p>
-    <div className="flex-main-end ">
+    <div className="flex-main-end">
       <img  src="/img/ftBien.jpg"  width="500" height="300"/>
     </div>
   </div> 
