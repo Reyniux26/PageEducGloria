0d499680302d85a85c2eefed625df205e95abc07
Reyna <canche26rey@gmail.com>
2024-02-11 22:12:03 -0600

    diseño responsivo

--- src/Componentes/Acerca.jsx
+++ src/Componentes/Acerca.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Acerca = () => {
   return (
-<div>
-  
-<div className="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary"style={{ display: 'flex' }}>
-  <div className="col-lg-6 px-0">
-    <br/>
-    <h3>Acerca de mí</h3>
-    <h2 className="display-4 fst-italic">
-      Bienvenidos
-    </h2>
-    <p  className="lead my-3">
-      Soy Gloria del Jesús Hernández Marín
+    <div>
+      <div className="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary" style={{ display: 'flex' }}>
+        <div className="col-lg-6 col-md-6 col-sm-12 px-0">
+          <br />
+          <h3>Acerca de mí</h3>
+          <h2 className="display-4 fst-italic">
+            Bienvenidos
+          </h2>
+          <p className="lead my-3">
+          Soy Gloria del Jesús Hernández Marín
       profesora investigadora de Tiempo Completo de la Facultad de Ciencias Educativas de la Universidad Autónoma del Carmen, Campeche, México. 
       Es doctora en Educación por la NOVA Southestern Univerity of Miami, Florida. Ha liderado diversos proyectos de investigación relacionados 
       con su línea de investigación, el Currículo y los procesos formativos en educación. 
@@ -21,18 +20,14 @@ const Acerca = () => {
       La maestra Hernández pertenece a la Asociación Latinoamérica de Filosofía de la Educación, y a la Red Iberoamericana de Investigación Sobre 
       Eficacia y Cambio Escolar. Su contribución a la formación del recurso humano es notable en las direcciones de tesis, y en la impartición de 
       educación continua para docentes desde el nivel básico, medio superior y superior.
-    </p>
-    
-  </div> 
-<div className="Foto1">
-      <img className='pres' src="/img/Gloria.jpg"  width="300" height="400"/>
+          </p>
+        </div>
+        <div className="Foto1">
+          <img className='pres' src="/img/Gloria.jpg" width="300" height="400" />
+        </div>
+      </div>
     </div>
-</div>
+  );
+};
 
-
-
-    </div>
-  )
-}
-
-export default Acerca
+export default Acerca;
